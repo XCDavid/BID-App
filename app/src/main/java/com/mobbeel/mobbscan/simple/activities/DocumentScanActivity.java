6f9832bfe1ccadaac542debf57861b18fa13bfa6
David M <dmolinero@teknei.com>
2017-06-26 13:43:47 -0500

    Comprobante de domicilio integrado con la librerira para recortar manualmente el documento

--- app/src/main/java/com/mobbeel/mobbscan/simple/activities/DocumentScanActivity.java
+++ app/src/main/java/com/mobbeel/mobbscan/simple/activities/DocumentScanActivity.java
@@ -1,11 +1,26 @@
 package com.mobbeel.mobbscan.simple.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.mobbeel.mobbscan.simple.R;
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
 
-public class DocumentScanActivity extends AppCompatActivity {
+import java.io.IOException;
+
+
+public class DocumentScanActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final int REQUEST_CODE = 99;
+    ImageButton takeDocumentPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +30,43 @@ public class DocumentScanActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(getResources().getString(R.string.document_scan_activity_name));
             invalidateOptionsMenu();
         }
+
+        takeDocumentPicture = (ImageButton) findViewById(R.id.ib_document_scan);
+        takeDocumentPicture.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ib_document_scan:
+                startScan(ScanConstants.OPEN_CAMERA);
+                break;
+        }
+    }
+
+    protected void startScan(int preference) {
+        Intent intent = new Intent(this, ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                getContentResolver().delete(uri, null, null);
+                takeDocumentPicture.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Bitmap convertByteArrayToBitmap(byte[] data) {
+        return BitmapFactory.decodeByteArray(data, 0, data.length);
     }
 }

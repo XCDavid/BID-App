9d67461d0a4cac67099d4870d2e1685971784529
David M <dmolinero@teknei.com>
2017-06-30 13:14:47 -0500

    Pantalla de LogIn y Confirmacion de pago

---
+++ app/src/main/java/com/mobbeel/mobbscan/simple/activities/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.mobbeel.mobbscan.simple.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobbeel.mobbscan.simple.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}

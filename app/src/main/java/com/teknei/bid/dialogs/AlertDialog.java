2430ef3410f36bd96c41751efec502adb27b9083
rgarciav <gar_ven_ruben@yahoo.com.mx>
2017-10-17 11:19:53 -0500

    Se agregaron validaciones para el mensaje DialogMessage, asi como configuraciones en los build.grade para tomar carpetas de librerias

--- app/src/main/java/com/teknei/bid/dialogs/AlertDialog.java
+++ app/src/main/java/com/teknei/bid/dialogs/AlertDialog.java
@@ -46,14 +46,14 @@ public class AlertDialog extends Dialog implements View.OnClickListener {
         txvMessage.setText(menssageIn);
 
         if (actionIn == ApiConstants.ACTION_TRY_AGAIN || actionIn == ApiConstants.ACTION_BLOCK_CANCEL_OPERATION ||
-                    actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL)
+                    actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL || actionIn == ApiConstants.ACTION_TRY_AGAIN_CONTINUE)
             okButton.setText    (activityOrigin.getString(R.string.message_ws_tray_again));
 
-        if (actionIn == ApiConstants.ACTION_LOG_OUT || actionIn == ApiConstants.ACTION_CANCEL_OPERATION ) {
+        if (actionIn == ApiConstants.ACTION_LOG_OUT || actionIn == ApiConstants.ACTION_CANCEL_OPERATION  || actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL) {
 
             cancelButton.setVisibility(View.VISIBLE);
 
-        } else if (actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL) {
+        } else if (actionIn == ApiConstants.ACTION_TRY_AGAIN_CONTINUE) {
 
             cancelButton.setVisibility(View.VISIBLE);
             cancelButton.setText(activityOrigin.getString(R.string.continue_message_dialog));
@@ -98,7 +98,7 @@ public class AlertDialog extends Dialog implements View.OnClickListener {
                     ((BaseActivity) activityOrigin).logOut();
                 }
 
-                if (actionIn == ApiConstants.ACTION_TRY_AGAIN || actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL) {
+                if (actionIn == ApiConstants.ACTION_TRY_AGAIN || actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL || actionIn == ApiConstants.ACTION_TRY_AGAIN_CONTINUE) {
                     //BORRAR
 //                    ((BaseActivity) activityOrigin).goNext();
                     //DES - COMENTAR
@@ -117,7 +117,7 @@ public class AlertDialog extends Dialog implements View.OnClickListener {
             case R.id.cancel_buttom:
                 dismiss();
 
-                if (actionIn == ApiConstants.ACTION_TRY_AGAIN || actionIn == ApiConstants.ACTION_TRY_AGAIN_CANCEL) {
+                if (actionIn == ApiConstants.ACTION_TRY_AGAIN_CONTINUE) {
                     ((BaseActivity) activityOrigin).goNext();
                 }
 
